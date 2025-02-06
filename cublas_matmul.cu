d9c9dc9b49465e450b66fcf17ea45ddaddd23686
Josiah Davis <josiah.j.davis@gmail.com>
2025-02-05 21:14:30 -0600

    Minor edits

--- cublas_matmul.cu
+++ cublas_matmul.cu
@@ -15,7 +15,7 @@ void printMatrix(const float* matrix, int rows, int cols, const char* name){
 
 void matrixMultiplyCpu(const float *A, const float *B, float *C, int M, int N, int K){
     for (int i = 0; i < M; i++){
-        for (int j = 0; j < N; i++){
+        for (int j = 0; j < N; j++){
             C[i * N + j] = 0.0f;
             for (int k = 0; k < K; k++){
                 C[i * N + j] += A[i * K + k] * B[k * N + j];
